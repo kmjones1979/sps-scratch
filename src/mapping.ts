79a8cf4491314df9652de0d49db9677ed86efa69
Kevin J <6829515+kmjones1979@users.noreply.github.com>
2024-03-11 00:08:31 -0700

    working but not indexing

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,7 +1,10 @@
-import { log, BigInt } from "@graphprotocol/graph-ts"; // install with yarn add @graphprotocol/graph-ts
-import * as assembly from "./pb/assembly"; // import the generated assembly module
+import { log, BigInt } from "@graphprotocol/graph-ts";
+import * as assembly from "./pb/assembly"; // Import the generated assembly module
 import { Contract } from "../generated/schema";
 
 export function handleBlock(blockBytes: Uint8Array): void {
     log.info("Handling block", []);
+    let decoded = assembly.eth.v1.Transactions.decode(blockBytes.buffer);
+    const transactions = decoded.transactions;
+    log.info("Transactions: {}", [transactions.length.toString()]);
 }
