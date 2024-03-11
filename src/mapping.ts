7b44666cd13592936570245f6f34a417d90e5d31
Kevin J <6829515+kmjones1979@users.noreply.github.com>
2024-03-11 00:29:31 -0700

    reverting a change

--- src/mapping.ts
+++ src/mapping.ts
@@ -4,7 +4,4 @@ import { Contract } from "../generated/schema";
 
 export function handleBlock(blockBytes: Uint8Array): void {
     log.info("Handling block", []);
-    let decoded = assembly.eth.v1.Transactions.decode(blockBytes.buffer);
-    const transactions = decoded.transactions;
-    log.info("Transactions: {}", [transactions.length.toString()]);
 }
